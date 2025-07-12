9060a8dc510074e93c28eac002fcead3491d7c6f
RammySubiate <140658238+RammySubiate@users.noreply.github.com>
2025-07-12 22:54:54 +0800

    Adding line graph

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 df = pd.read_csv("mock_health_data.csv")
 
-print(df.head())
+df["date"] = pd.to_datetime(df["date"])
+df["calories_goal"] = 2000
+x = df[df["date"] > "2025-06-15"]
+dates = x["date"]
+y = x["calories_burned"]
+z = x["calories_consumed"]
+zy = x["calories_goal"]
+
+
+
+
+plt.plot(dates,y)
+plt.plot(dates,z)
+plt.plot(dates, zy)
+
+
+plt.show()
+
+
+
